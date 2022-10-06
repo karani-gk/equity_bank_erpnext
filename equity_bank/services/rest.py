a075a9b50cb6173faf6f36a5c543f6a0a6a4735c
karani-gk <geoffrey.kamundi@gmail.com>
2022-10-06 23:16:48 +0300

    first commit

---
+++ equity_bank/services/rest.py
@@ -0,0 +1,42 @@
+import frappe
+
+
+
+@frappe.whitelist(allow_guest=True)
+def validate_transaction(account, username, password):
+    
+    bill_information = {
+        "amount": 100,
+        "billName": "Rent",
+        "billNumber": "HAV302",
+        "billerCode": "247247",
+        "createdOn": "2022-10-07",
+        "currencyCode": "KES",
+        "customerName": "Geoffrey Karani",
+        "customerRefNumber": "HAV302",
+        "description": "",
+        "dueDate": "",
+        "expiryDate": "",
+        "type": 1
+    }
+    
+    return bill_information
+    
+
+
+@frappe.whitelist(allow_guest=True)
+def register_payment_notification(**kwargs):
+    
+    
+    if kwargs['billNumber'] == "HAV1234":
+        response = {
+            "responseCode": "OK",
+            "responseMessage": "SUCCESSFUL"
+        }
+    else:
+        response = {
+            "responseCode": "OK",
+            "responseMessage": "DUPLICATETRANSACTION"
+        }
+        
+    return response
